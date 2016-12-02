97a1724816f1fc3d6aa9ad917fb730f7029fb11d
Justin Blake <justin.blake.dev@gmail.com>
2016-12-02 10:39:22 -0700

    Auth Routes

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -4,21 +4,42 @@ var Owner = require('../models/owner');
 var jwt = require('jsonwebtoken');
 var config = require('../../config');
 
-var test = "test";
+// create a new user
 
+authRoutes.route('/signup')
+    .post(function(req, res) {
+        Owner.find({username: req.body.username}, function(err, existingUsers) {
+            if(err) {
+                return res.status(500).send(err);
+            } else if (existingUsers.length) {
+                return res.status(401).json({success: false, message: 'Username is already in use'});
+            } else {
+                var newOwner = new Owner(req.body);
+                newOwner.save(function(err, ownerObject) {
+                    if(err) {
+                        res.status(500).send(err);
+                    } else{
+                        res.send({user: ownerObject, message: "Successfully created a new user.", success: true});
+                    }
+                });
+            }
+        })
+    });
 
-authRoutes.route('/')
-    .get(function(req, res) {
+// authenticate an existing user
 
-        res.send(test);
-
-        // Owner.find({}, function(err, response) {
-        //     if(err){
-        //         res.status(500).send(err);
-        //     } else{
-        //         res.send(response);
-        //     }
-        // })
+authRoutes.route('/login')
+    .post(function(req, res) {
+        Owner.findOne({username: req.body.username, password: req.body.password}, function(err, owner) {
+            if(err) {
+                return res.status(500).send(err);
+            } else if (!owner) {
+                return res.status(403).send({success: false, message: "Username and/or password is not valid"});
+            } else {
+                var token = jwt.sign(owner.toObject(), config.secret, {expiresIn: "24h"});
+                res.json({token: token, user: owner.toObject(), success: true, message: "Here is your token"});
+            }
+        })
     });
 
 
