c242134714c466770104b24abcd3d98bdf8f2087
Justin Blake <justin.blake.dev@gmail.com>
2016-12-01 17:15:16 -0700

    update

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,8 @@ app.controller("mainCtrl", ["$scope", "$http", function ($scope, $http) {
     $scope.getHello = function () {
             $http.get("/auth")
                 .then(function (response) {
-                    $scope.hello = response.data
-                    console.log(data)
+                    $scope.hello = response.data;
+                    console.log($scope.hello)
                 })
         };
         //    $scope.getHello();
