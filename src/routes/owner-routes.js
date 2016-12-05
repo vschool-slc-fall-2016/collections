fd8b97de61ce2a81d6b1fdf4df4aaacd6c720ea7
Justin Blake <justin.blake.dev@gmail.com>
2016-12-05 10:19:47 -0700

    Change Database name

---
+++ src/routes/owner-routes.js
@@ -0,0 +1,9 @@
+// var express = require('express');
+// var ownerRouter = express.Router();
+// var ItemObject = require('../models/item');
+// var Owner = require('../models/owner');
+//
+// ownerRouter.route('/')
+//     .get(function (req, res) {
+//         Owner.find()
+//     })
