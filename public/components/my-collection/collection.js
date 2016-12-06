2160ed2391766a72b21f4814de8f3dd179abb1b4
Stetson Peck <stetson.peck@gmail.com>
2016-12-06 14:19:34 -0700

    my collections

--- public/components/my-collection/collection.js
+++ public/components/my-collection/collection.js
@@ -1,20 +1,24 @@
 var app = angular.module("App");
 
-app.controller("collectionCtrl", ["$scope", "HttpService", function($scope, HttpService) {
+app.controller("collectionCtrl", ["$scope", "HttpService", function ($scope, HttpService) {
 
+    $scope.myItems = {};
     //get item
-    $scope.getItem = function(gotItem) {
-        HttpService.getItems();
-        console.log("get item is working");
-    };   
+    (function getItem() {
+        HttpService.getItems().then(function (myItems) {
+            $scope.myItems = myItems;
+            console.log("get item is working");
+        })
+    })();
+
     //update item
-    $scope.updateItem = function(itemId) {
+    $scope.updateItem = function (itemId) {
         HttpService.updateSpecificItem();
         console.log("update item is working");
     };
     //delete Item
-    $scope.deleteItem = function(itemId) {
+    $scope.deleteItem = function (itemId) {
         HttpService.deleteItem();
         console.log("deleting item is working");
-    } ;
+    };
 }]);
